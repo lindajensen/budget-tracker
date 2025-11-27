cc07d194be06312c66081138b643c3fc181f3ef7
Linda Jensen <linda.jensen81@me.com>
2025-11-27 09:11:12 +0100

    Set up React Router with basic routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,28 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
 import "./App.css";
 
+// Router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/dashboard" element={<div>Dashboard</div>} />
+      </Route>
+    </>
+  )
+);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
