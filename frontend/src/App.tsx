a71e66f3c13c3372ac140888e59894b76e3b3b04
Linda Jensen <linda.jensen81@me.com>
2025-11-27 12:09:43 +0100

    Add HomePage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,13 +7,14 @@ import {
 
 import "./App.css";
 import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
 
 // Router
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<MainLayout />}>
-        <Route index element={<div>Home</div>} />
+        <Route index element={<HomePage />} />
         <Route path="/login" element={<div>Login</div>} />
         <Route path="/register" element={<div>Register</div>} />
         <Route path="/dashboard" element={<div>Dashboard</div>} />
