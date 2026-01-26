06af7c90fd40486eac584544032c57cab3bcaf15
Linda Jensen <linda.jensen81@me.com>
2026-01-26 08:55:06 +0100

    Add responsive navbar with mobile menu

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
 
+import Header from "../components/Header";
+
 function MainLayout() {
   return (
     <div className="layout">
+      <Header />
       <main className="layout__content">
         <Outlet />
       </main>
